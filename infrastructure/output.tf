52b073362d5d6ec4328732a568483d2fdb6a8159
Franek Suszko <franeksu@gmail.com>
2024-11-28 08:56:26 +0100

    Fix main outputs

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -3,7 +3,12 @@
 #   description = "The connection endpoint for the RDS PostgreSQL instance"
 # }
 
-output "ec2_public_ip" {
-  value       = module.ec2.instance_public_ip
-  description = "Public IP address of the EC2 instance"
+output "first_public_ip" {
+  value       = module.ec2.first_instance_public_ip
+  description = "Public IP address of the first manager EC2 instance"
+}
+
+output "other_public_ip" {
+  value       = module.ec2.other_instance_public_ip
+  description = "Public IP address of other manager EC2 instances"
 }
